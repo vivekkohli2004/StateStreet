de9a0f9e4ccea30f5c7fbb078da9e14fd89683ec
vivekkohli2004 <vivekkohli2004@gmail.com>
2021-08-22 23:58:35 -0400

    Update AppConstants.java

--- StateStreet/src/com/statestreet/interview/design/util/AppConstants.java
+++ StateStreet/src/com/statestreet/interview/design/util/AppConstants.java
@@ -4,7 +4,7 @@ public class AppConstants {
 	
 	public static final String IP_ADDRESS="192.168.1.243";
 	public static final int PORT= 8087;
-	public static final int QUEUE_MAX_SIZE= 10000;
+	public static final int QUEUE_MAX_SIZE= 1000;
 	public static final int NO_OF_THREADS_CLIENT_PUBLISHER= 1;
 	public static final int NO_OF_THREADS_CLIENT_CONSUMER= 5;
 	public static final int NO_OF_THREADS_SERVER_CONSUMER= 15;
